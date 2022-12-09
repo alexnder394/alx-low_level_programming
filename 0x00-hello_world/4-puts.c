61287968761b4f9d4d7c53653c186cfd56322c02
M. Reda <mohamedreda11@gmail.com>
2022-12-09 21:22:03 +0200

    added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  * Print "Programming is like building
  * a multilingual puzzle" followed by a new line
  */
-int main(void)
+int main(void) /*main function prints text only */
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+i	puts("\"Programming is like building a multilingual puzzle");
+	retiurn (0);
 }
