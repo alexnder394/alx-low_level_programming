041727c07b4f7478bb6f0ca8036a05324b8ae4fb
M. Reda <mohamedreda11@gmail.com>
2022-12-09 21:58:46 +0200

    modified file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,9 @@
 /**
  * main - Entry point
  *
- * Print "Programming is like building
- * a multilingual puzzle" followed by a new line
+ * Return: Always = 0.
  */
-int main(void) /* main with and Return: Always 0 (Succes) */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
