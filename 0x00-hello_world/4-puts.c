6015f5e0ad33c88388b8c23d1fe21df4bbafa3bb
M. Reda <mohamedreda11@gmail.com>
2022-12-09 21:31:20 +0200

    modified 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,8 @@
  * Print "Programming is like building
  * a multilingual puzzle" followed by a new line
  */
-int main(void) /*main function prints text only */
+iint main(void) /* main with and Return: Always 0 (Succes) */
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
 	return (0);
 }
