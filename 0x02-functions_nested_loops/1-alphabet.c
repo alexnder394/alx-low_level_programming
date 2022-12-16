4885ce2251e1656147a73200a22fcb91cdfca35d
M. Reda <mohamedreda11@gmail.com>
2022-12-17 01:17:38 +0200

    solved 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,6 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c);
-/**
  * print_alphabet - prints all alphabet in lower case
  *
  */
