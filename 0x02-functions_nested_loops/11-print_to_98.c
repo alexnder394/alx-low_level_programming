0cc9e59aacaa3017bd031b3afe5eb15dbbdad3b4
M. Reda <mohamedreda11@gmail.com>
2022-12-17 14:57:10 +0200

    fixed error

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,15 +19,14 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i < 98)
-				printf(",");
-			printf(" ");
+				printf(", ");
 		}
 	}
 	else
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
-			if (i < 98)
+			if (i > 98)
 				printf(", ");
 		}
 	printf("\n");
