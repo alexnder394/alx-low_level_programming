ea776ce792c0e7f1039a172c140e957f357e242b
M. Reda <mohamedreda11@gmail.com>
2022-12-17 03:07:49 +0200

    added solution to file 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks if character c is alphabetic
+ *
+ * @c: an integer value of a character
+ *
+ * Return: 1 if true, 0 if false
+ *
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	return (0);
+}
