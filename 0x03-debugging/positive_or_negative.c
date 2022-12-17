2fc859e046bde5a6010c1a8a5c6ccd681e6f19e7
M. Reda <mohamedreda11@gmail.com>
2022-12-17 21:34:26 +0200

    fixed errors

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -5,14 +5,14 @@
 * postitive_or_negative - check if num is psve or ngve
 * @n: integer to be tested
 */
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
 
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
+if (i > 0)
+printf("%d is positive\n", i);
+else if (i == 0)
+printf("%d is zero\n", i);
 else
-printf("%d is negative\n", n);
+printf("%d is negative\n", i);
 }
 
