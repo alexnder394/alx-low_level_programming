7f00f5f032a3169949c7d1096009d9093fcb008e
M. Reda <mohamedreda11@gmail.com>
2022-12-29 01:15:39 +0200

    fixed file 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,11 @@ void print_most_numbers(void)
 	int i;
 
 	i = 0;
-	while (i < 10 && (i != 2 || i != 4))
+	while (i < 10)
 	{
-		_putchar(i + '0');
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+		i++;
 	}
 	_putchar('\n');
 }
