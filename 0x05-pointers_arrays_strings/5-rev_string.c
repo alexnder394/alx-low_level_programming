ad1f74967c4a35c766b16ac0e82baca48a1a337b
M. Reda <mohamedreda11@gmail.com>
2023-01-14 17:25:17 +0200

    added file 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - reverses a string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+int len;
+int i;
+
+len = 0;
+i = 0;
+char tmp;
+while (s[i++])
+len++;
+
+for (i = len - 1; i >= len / 2; i--)
+{
+tmp = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = tmp;
+}
+}
