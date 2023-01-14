3e4015566e8926e5ac739855014cc73676197517
M. Reda <mohamedreda11@gmail.com>
2023-01-14 05:55:42 +0200

    added file 2 fixes

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,17 @@
+#include "main.h"
+
 /**
  * _strlen - swaps the values of two integers
  * @s: string
+ *
  * Return: length of a string
  */
 int _strlen(char *s)
 {
-return (strlen(s));
+int len;
+for (len = 0; *s != '\0'; len++)
+{
+s++;
+}
+return (len);
 }
