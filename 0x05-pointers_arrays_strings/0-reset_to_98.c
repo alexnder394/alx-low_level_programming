a2221ac082e4249a5dd690a7828219dc07e783de
M. Reda <mohamedreda11@gmail.com>
2023-01-14 02:21:42 +0200

    fixed errors in coding style in file 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * reset_to_98: akes a pointer to an int as parameter 
+ * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98
  * @n: pointer to integer
- * 
+ *
  */
 void reset_to_98(int *n)
 {
-     *n = 98;
+*n = 98;
 }
