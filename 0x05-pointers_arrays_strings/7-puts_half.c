d3c48bbcb2c8c4bc296986b6a7adc518574eb023
M. Reda <mohamedreda11@gmail.com>
2023-01-14 17:59:16 +0200

    added file 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,20 @@ void puts_half(char *str)
 {
 int i;
 int len;
+int n;
 
 i = 0;
 len = 0;
+n = 0;
 while (str[i++])
 len++;
 
-for (i = len / 2; i < len; i++)
+if ((len % 2) == 0)
+n = len / 2;
+else
+n = (len - 1) / 2;
+
+for (i = n; i < len; i++)
 putchar(str[i]);
 putchar('\n');
 }
