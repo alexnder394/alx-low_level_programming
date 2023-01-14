47396a15cb0afee77476dd03af9ef9c83bc1a056
M. Reda <mohamedreda11@gmail.com>
2023-01-14 17:37:00 +0200

    added file 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * puts2 - prints every other character of a string
+ * @str: string
+ */
+void puts2(char *str)
+{
+int i;
+int len;
+
+i = 0;
+len = 0;
+while (str[i++])
+{
+len++;
+}
+for (i = 0; i < len; i++)
+{
+if (i % 2 == 0)
+putchar(str[i]);
+}
+}
